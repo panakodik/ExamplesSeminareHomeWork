a0f470f62abf40c0c4792a9329517fe80328e2c9
Aleksandr <pandom@yandex.ru>
2023-01-15 02:09:54 +0300

    E-S-HomeWork-04 Найти максимальное из трех чисел

---
+++ E-S-HomeWork-04/Program.cs
@@ -0,0 +1,17 @@
+﻿// Даны два числа. Показать большее и меньшее числа.
+Console.WriteLine("Введите последовательно три числа. Программа определит большее ");
+Console.WriteLine("Введите значение первого числа ");
+double numberA = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение второго числа ");
+double numberB = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение третьего числа ");
+double numberC = Convert.ToDouble(Console.ReadLine());
+
+double max = numberA;
+
+if (numberA > max ) max = numberA;
+if (numberB > max ) max = numberB;
+if (numberC > max ) max = numberC;
+
+Console.Write("Наибольшее число равно ");
+Console.WriteLine(max);
