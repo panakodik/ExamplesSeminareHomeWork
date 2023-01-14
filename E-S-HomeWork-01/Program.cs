8eb412a067bf2af683a885ed1df2afbc5d523d19
Aleksandr <pandom@yandex.ru>
2023-01-15 02:04:06 +0300

    E-S-HomeWork-01 Квадрат числа

---
+++ E-S-HomeWork-01/Program.cs
@@ -0,0 +1,7 @@
+﻿// Вывести квадрат числа
+Console.WriteLine("Найти квадрат числа ");
+Console.WriteLine("Введите число ");
+double numberA = Convert.ToDouble(Console.ReadLine());
+double result = numberA * numberA;
+Console.Write("Итог: ");
+Console.WriteLine(result);
