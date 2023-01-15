0bc182c4765bd188f783bfbed6d0a6966c84ec59
Aleksandr <pandom@yandex.ru>
2023-01-16 01:19:50 +0300

    E-S-HomeWork-02 Первое квадратом второго

---
+++ E-S-HomeWork-02/Program.cs
@@ -0,0 +1,15 @@
+﻿// По двум заданным числам проверять является ли первое квадратом второго
+Console.WriteLine("По двум заданным числам проверим является ли первое квадратом второго ");
+Console.WriteLine("Введите первое число ");
+double numberA = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+double numberB = Convert.ToDouble(Console.ReadLine());
+
+if(numberB * numberB == numberA)
+{
+    Console.WriteLine($"Первое число *{numberA}* является квадратом второго числа *{numberB}*");
+}
+else
+{
+    Console.WriteLine($"Первое число *{numberA}* не является квадратом второго числа *{numberB}*");
+}
