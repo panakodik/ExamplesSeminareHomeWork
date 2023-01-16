811c52be580d55cda0cc99d7359c32bfade063de
Aleksandr <pandom@yandex.ru>
2023-01-17 00:33:50 +0300

    E-S-HomeWork-05 Является ли число четным

---
+++ E-S-HomeWork-05/Program.cs
@@ -0,0 +1,16 @@
+﻿// Выяснить является ли число чётным
+Console.WriteLine("Выяснить является ли число чётным");
+int numberA = new Random().Next(1,100);
+
+if(numberA % 2 == 0)
+{
+    Console.Write("Число ");
+    Console.Write(numberA);
+    Console.WriteLine(" четное");
+}
+else
+{
+    Console.Write("Число ");
+    Console.Write(numberA);
+    Console.WriteLine(" не четное");
+}
