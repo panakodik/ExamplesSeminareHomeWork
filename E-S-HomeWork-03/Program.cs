7568b967eb7d5a17e6874eca893387858414fbca
Aleksandr <pandom@yandex.ru>
2023-01-19 01:13:38 +0300

    E-S-HomeWork-03 Дни недели

---
+++ E-S-HomeWork-03/Program.cs
@@ -0,0 +1,34 @@
+﻿// По заданному номеру дня недели вывести его название
+//int day;
+Console.Write("Введите номер дня недели ");
+int day = int.Parse(Console.ReadLine());
+
+if(day == 1) 
+{
+    Console.WriteLine("Понедельник");
+}
+else if(day == 2) 
+{
+    Console.WriteLine("Вторник");
+}
+else if(day == 3) 
+{
+    Console.WriteLine("Среда");
+}
+else if(day == 4) 
+{
+    Console.WriteLine("Четверг");
+}
+else if(day == 5) 
+{
+    Console.WriteLine("Пятница");
+}
+else if(day == 6) 
+{
+    Console.WriteLine("Суббота");
+}
+else if(day == 7) 
+{
+    Console.WriteLine("Воскресенье");
+}
+else Console.WriteLine("Такого дня недели не существует");
