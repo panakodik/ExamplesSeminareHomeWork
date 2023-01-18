448a994e90e2da65fe82a45ea861930ef50b2e41
Aleksandr <pandom@yandex.ru>
2023-01-19 02:46:46 +0300

    E-S-HomeWork-06 Четные числа диапазона

---
+++ E-S-HomeWork-06/Program.cs
@@ -0,0 +1,15 @@
+﻿// Показать четные числа от 1 до N
+int n = 1;
+Console.Write("Введите конечное число ");
+int m = int.Parse(Console.ReadLine());
+if(n == 1)
+{
+    n += 1;
+}
+while(n <= m)
+{
+    Console.Write( n + " " );
+    n += 2;
+}
+Console.WriteLine();
+    Console.WriteLine("Все четные числа заданного диапазона");
