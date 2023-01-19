5f01bf238efc4452c68e856343112eff9c2efcc7
Aleksandr <pandom@yandex.ru>
2023-01-20 00:20:33 +0300

    E-S-HomeWork-12 Третья цифра есть или нет

---
+++ E-S-HomeWork-12/Program.cs
@@ -0,0 +1,14 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+Console.Clear();
+int number = new Random().Next(1,1000);
+Console.WriteLine("Дано число " + number);
+int a = number % 10;
+int b = number % 1000 / 100;
+if(b > 0)
+{
+    Console.WriteLine("Третья цифра числа слева направо " + b);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
