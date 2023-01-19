b6f6276d738a1174fefb6dfc5444bec310005ed2
Aleksandr <pandom@yandex.ru>
2023-01-19 04:04:20 +0300

    E-S-HomeWork-11 Кратно ли число заданному

---
+++ E-S-HomeWork-11/Program.cs
@@ -0,0 +1,15 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+Console.Clear();
+Console.WriteLine("Выясняем, кратно ли число заданному. Введите делитель ");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число, кратность которыго нужно узнать ");
+int numberB = int.Parse(Console.ReadLine());
+if(numberB % numberA == 0)
+{
+    Console.WriteLine("Число " + numberB + " кратно " + numberA);
+}
+else
+{
+    int numberC = numberB % numberA;
+    Console.WriteLine("Число " + numberB + " не кратно " + numberA + " с остатком " + numberC);
+}
