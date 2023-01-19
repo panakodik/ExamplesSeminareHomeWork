bb241c3583dcf5cad637f97ab6522b75cba9db4b
Aleksandr <pandom@yandex.ru>
2023-01-19 03:37:40 +0300

    E-S-HomeWork-10 Удаляем вторую цифру из трехзначного числа

---
+++ E-S-HomeWork-10/Program.cs
@@ -0,0 +1,9 @@
+﻿// Удалить вторую цифру трёхзначного числа
+Console.Clear();
+int number = new Random().Next(100,999);
+Console.WriteLine("Дано число " + number);
+int a = number % 10;
+int b = number % 100 / 10;
+int c = number % 1000 / 100;
+int d = c * 10 + a;
+Console.WriteLine("Стираем вторую цифру из числа и получаем " + d);
