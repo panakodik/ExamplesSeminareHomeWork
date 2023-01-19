1daa438a59441ca9d4d6e908479c3313e3be9d73
Aleksandr <pandom@yandex.ru>
2023-01-19 03:27:30 +0300

    E-S-HomeWork-09 Наибольшая цифра числа

---
+++ E-S-HomeWork-09/Program.cs
@@ -0,0 +1,13 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+int number = new Random().Next(10,99);
+Console.WriteLine("Дано число " + number);
+int a = number % 10;
+int b = number % 100 / 10;
+if(a > b)
+{
+    Console.WriteLine("Наибольшая цифра числа " + a);
+}
+else
+{
+    Console.WriteLine("Наибольшая цифра числа " + b);
+}
