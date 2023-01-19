9f45e792b6cb481b69f7dc1a0e00e679b2a8452a
Aleksandr <pandom@yandex.ru>
2023-01-19 03:13:38 +0300

    E-S-HomeWork-08 вторая цифра трехзначного числа

---
+++ E-S-HomeWork-08/Program.cs
@@ -0,0 +1,5 @@
+﻿//Показать вторую цифру трёхзначного числа
+Console.Write("Введите трехзначное число ");
+int number = int.Parse(Console.ReadLine());
+int n = number % 100 / 10;
+Console.WriteLine("Вторая цифра трехзначного числа: " + n);
