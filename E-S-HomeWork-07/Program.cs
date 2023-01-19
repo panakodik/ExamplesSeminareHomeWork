4583a51d311b5637dd3e3ad5d84e2449dc2fb768
Aleksandr <pandom@yandex.ru>
2023-01-19 03:06:15 +0300

    E-S-HomeWork-07 Последняя цифра

---
+++ E-S-HomeWork-07/Program.cs
@@ -0,0 +1,5 @@
+﻿//Показать последнюю цифру трёхзначного числа
+Console.Write("Введите трехзначное число ");
+int number = int.Parse(Console.ReadLine());
+int n = number % 10;
+Console.WriteLine("Последняя цифра трехзначного числа: " + n);
